b614430cbe264153267e8709e6093b69ba728d74
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-07-14 22:05:02 +0530

    Add MathLibrary header for scientific calculations

---
+++ C++/ScientificCalculator/src/MathLibrary.h
@@ -0,0 +1,25 @@
+#ifndef MATHLIBRARY_H
+#define MATHLIBRARY_H
+
+#include <string>
+#include <vector>
+#include <map>
+#include <functional>
+
+
+// Function type:
+// Allows both single and multi argument functions
+
+using MathFunction =
+    function<double(vector<double>)>;
+
+
+
+extern map<string, MathFunction> MATH_LIB;
+
+
+
+void initializeMathLibrary();
+
+
+#endif
