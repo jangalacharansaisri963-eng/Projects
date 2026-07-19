6877687a97867a505b1aa50fb0c10225d387f810
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-07-19 08:57:10 +0530

    Refactor ConstantLibrary to use Constants class

--- Java/ScientificCalculator/src/library/ConstantLibrary.java
+++ Java/ScientificCalculator/src/library/ConstantLibrary.java
@@ -1,43 +1,29 @@
 package library;
 
-/*
- * ConstantLibrary
- *
- * Registers all mathematical constants.
- */
+import constants.Constants;
 
 public class ConstantLibrary {
 
     public static void register() {
 
-        FunctionLibrary.register(
+        FunctionLibrary.registerConstant(
                 "pi",
-                args -> Math.PI
+                Constants.PI
         );
 
-        FunctionLibrary.register(
-                "PI",
-                args -> Math.PI
-        );
-
-        FunctionLibrary.register(
+        FunctionLibrary.registerConstant(
                 "e",
-                args -> Math.E
+                Constants.E
         );
 
-        FunctionLibrary.register(
-                "E",
-                args -> Math.E
-        );
-
-        FunctionLibrary.register(
+        FunctionLibrary.registerConstant(
                 "phi",
-                args -> (1.0 + Math.sqrt(5.0)) / 2.0
+                Constants.PHI
         );
 
-        FunctionLibrary.register(
-                "R15",
-                args -> constants.Constants.R15
+        FunctionLibrary.registerConstant(
+                "r15",
+                Constants.R15
         );
 
     }
