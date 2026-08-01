ee6e7c350a4ebcd844da9cd5bd5396d288b166e2
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-08-01 13:47:53 +0530

    Add various mathematical functions to basic_math.py

--- Python/ScientificCalculator/functions/basic_math.py
+++ Python/ScientificCalculator/functions/basic_math.py
@@ -179,3 +179,157 @@ def reverse_number(n):
         reversed_number *= -1
 
     return reversed_number
+
+def apsum(a, b):
+    return (a + b) * b / 2
+
+
+def apsub(a, b):
+    return (a - b) * b / 2
+
+
+def apterm(a, d, n):
+    return a + (n - 1) * d
+
+
+def apsum(a, d, n):
+    return n * (2 * a + (n - 1) * d) / 2
+
+
+def gpsum(a, r, n):
+    if r == 1:
+        return a * n
+    return a * (r ** n - 1) / (r - 1)
+
+
+def gpterm(a, r, n):
+    return a * r ** (n - 1)
+
+
+def average(a, b):
+    return (a + b) / 2
+
+
+def percentage(value, percent):
+    return value * percent / 100
+
+
+def percentof(a, b):
+    return (a / b) * 100
+
+
+def increase(value, percent):
+    return value * (1 + percent / 100)
+
+
+def decrease(value, percent):
+    return value * (1 - percent / 100)
+
+
+def ratio(a, b):
+    return a / b
+
+
+def proportion(a, b, c):
+    return (b * c) / a
+
+
+def simple_interest(p, r, t):
+    return (p * r * t) / 100
+
+
+def amount(p, r, t):
+    return p + simple_interest(p, r, t)
+
+
+def compound_amount(p, r, n, t):
+    return p * (1 + r / (100 * n)) ** (n * t)
+
+
+def compound_interest(p, r, n, t):
+    return compound_amount(p, r, n, t) - p
+
+
+def factorial(n):
+    result = 1
+
+    for i in range(1, n + 1):
+        result *= i
+
+    return result
+
+
+def triangular(n):
+    return n * (n + 1) / 2
+
+
+def squaresum(n):
+    return n * (n + 1) * (2 * n + 1) / 6
+
+
+def cubesum(n):
+    return (n * (n + 1) / 2) ** 2
+
+
+def sumofintegers(a, b):
+    return (a + b) * (b - a + 1) / 2
+
+
+def distance(speed, time):
+    return speed * time
+
+
+def speed(distance, time):
+    return distance / time
+
+
+def time(distance, speed):
+    return distance / speed
+
+
+def work(force, distance):
+    return force * distance
+
+
+def power(work, time):
+    return work / time
+
+
+def kineticenergy(mass, velocity):
+    return 0.5 * mass * velocity ** 2
+
+
+def potentialenergy(mass, gravity, height):
+    return mass * gravity * height
+
+
+def density(mass, volume):
+    return mass / volume
+
+
+def mass(density, volume):
+    return density * volume
+
+
+def volume(mass, density):
+    return mass / density
+
+
+def pythagoras(a, b):
+    return (a ** 2 + b ** 2) ** 0.5
+
+
+def hypotenuse(a, b):
+    return (a ** 2 + b ** 2) ** 0.5
+
+
+def quadratic(a, b, c):
+    discriminant = b ** 2 - 4 * a * c
+
+    if discriminant < 0:
+        return None
+
+    return (
+        (-b + discriminant ** 0.5) / (2 * a),
+        (-b - discriminant ** 0.5) / (2 * a)
+    )
